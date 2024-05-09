38ff854f7bb877f47a6ec3dc1ac3bb2136ccbe2a
Ibrahem Younas <younasibrahem@gmail.com>
2024-05-09 17:48:05 -0400

    passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [question, postQuestions]=useState([])
+  const postNewQuestion = (newQuestion) => {
+    postQuestions([...question, newQuestion])
+  }
+
+const onDelete= (data) => {
+  const getQuestion = question.filter(question => question.id !== data.id)
+  postQuestions(getQuestion)
+};
+ 
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm postNewQuestion={postNewQuestion}  /> : <QuestionList  onDelete={onDelete}  />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+const [questions, postQuestion]=useState([])
+useEffect(() => {
+  fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then(data => {
+      postQuestion(data);
+    })
+}
+,[]);
+const getQuestions= questions.map((value) => (
+  <QuestionItem 
+  key={value.id}
+  question={value}
+  onUpdate={handleUpdateQuestion}
+  onDelete={handleDeleteQuestion}
+  />
+))
+
+function handleUpdateQuestion(updatedQuestion){
+  const updatedQuestions = questions.map(question => {
+    if(question.id === updatedQuestion.id){
+      return updatedQuestion;
+    }
+    else{
+      return question;
+    }
+  })
+  postQuestion(updatedQuestions);
+}
+function handleDeleteQuestion(deletedQuestion){
+  const updatedQuestions = questions.filter(question => question.id!==deletedQuestion.id);
+  postQuestion(updatedQuestions);
+}
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul> 
+       {getQuestions}
+        </ul>
     </section>
   );
 }
